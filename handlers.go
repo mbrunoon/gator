b44cd1bebd19132b9eec16c526194726036cb8ae
Marcos Bruno <contato@marcosbruno.com>
2025-01-14 08:24:30 +0000

    Ch2.L4: add reset users

--- handlers.go
+++ handlers.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"context"
 	"errors"
 	"fmt"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/mbrunoon/gator/internal/database"
 )
 
 func handlerLogin(s *state, cmd command) error {
@@ -12,12 +17,62 @@ func handlerLogin(s *state, cmd command) error {
 
 	username := cmd.Args[0]
 
-	err := s.config.SetUser(username)
+	_, err := s.db.GetUser(context.Background(), username)
 	if err != nil {
-		return fmt.Errorf("couldn't set user: %w", err)
+		return errors.New("user not found")
+	}
+
+	err = s.config.SetUser(username)
+	if err != nil {
+		return errors.New("could not set current user")
 	}
 
 	fmt.Printf("New current username: %s\n", s.config.CurrentUserName)
 
 	return nil
+}
+
+func handlerRegister(s *state, cmd command) error {
+	if len(cmd.Args) != 1 {
+		return errors.New("invalid command")
+	}
+
+	newUserName := cmd.Args[0]
+
+	user, err := s.db.CreateUser(context.Background(), database.CreateUserParams{
+		ID:        uuid.New(),
+		Name:      newUserName,
+		CreatedAt: time.Now().UTC(),
+		UpdatedAt: time.Now().UTC(),
+	})
+
+	if err != nil {
+		return fmt.Errorf("could not create user %s: %w", newUserName, err)
+	}
+
+	err = s.config.SetUser(user.Name)
+	if err != nil {
+		return fmt.Errorf("Could not set user %s", user.Name)
+	}
+
+	fmt.Println("User created:")
+	printUser(user)
+
+	return nil
+}
+
+func handlerReset(s *state, _ command) error {
+
+	err := s.db.ResetUsers(context.Background())
+	if err != nil {
+		return fmt.Errorf("could not clean user table: %w", err)
+	}
+
+	fmt.Println("User table is clean!")
+	return nil
+}
+
+func printUser(user database.User) {
+	fmt.Printf(" * ID:		%v\n", user.ID)
+	fmt.Printf(" * Name: 	%v\n", user.Name)
 }
--- main.go
+++ main.go
@@ -1,31 +1,48 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"os"
 
 	"github.com/mbrunoon/gator/internal/config"
+	"github.com/mbrunoon/gator/internal/database"
+
+	_ "github.com/lib/pq"
 )
 
 type state struct {
+	db     *database.Queries
 	config *config.Config
 }
 
 func main() {
+
 	cfg, err := config.Read()
 	if err != nil {
 		log.Fatalf("Error reading config: %v", err)
 	}
 
+	db, err := sql.Open("postgres", cfg.DBURL)
+	if err != nil {
+		log.Fatal("Error load database: $v", err)
+	}
+	defer db.Close()
+	dbQueries := database.New(db)
+
 	programState := &state{
+		db:     dbQueries,
 		config: &cfg,
 	}
 
 	cmds := commands{
 		registeredCommands: make(map[string]func(*state, command) error),
 	}
+
 	cmds.register("login", handlerLogin)
+	cmds.register("register", handlerRegister)
+	cmds.register("reset", handlerReset)
 
 	if len(os.Args) < 2 {
 		log.Fatal("Usage: cli <command [args...]>")
